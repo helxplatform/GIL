5ca9e5c452c5d069ece639f8ccf34c1f96f277df
satusky <50336340+satusky@users.noreply.github.com>
2022-05-27 11:42:19 -0400

    Update gpu_test.py

--- gpu_test.py
+++ gpu_test.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 if __name__ == "__main__":
-    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
     assert x_train.shape == (50000, 32, 32, 3)
     assert x_test.shape == (10000, 32, 32, 3)
     assert y_train.shape == (50000, 1)
