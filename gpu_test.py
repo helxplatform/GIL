972de7f83291a102511dc398d54be4d1015e9fa5
satusky <50336340+satusky@users.noreply.github.com>
2022-05-27 11:34:25 -0400

    Simple script to test gpu connectivity

---
+++ gpu_test.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+if __name__ == "__main__":
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    assert x_train.shape == (50000, 32, 32, 3)
+    assert x_test.shape == (10000, 32, 32, 3)
+    assert y_train.shape == (50000, 1)
+    assert y_test.shape == (10000, 1)
+
+    model = tf.keras.applications.VGG16(weights=None, input_shape=(32, 32, 1), classes=10)
+    print(model.summary())
+
+    model.compile(loss="sparse_categorical_crossentropy")
+    model.fit(x_train[:5, ...], y_train[:5], verbose=True)
