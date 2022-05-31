1cf1ae09a021500fd77f851da9488a0090870380
satusky <50336340+satusky@users.noreply.github.com>
2022-05-31 13:20:26 -0400

    Unit conversion in model size

--- src/utility.py
+++ src/utility.py
@@ -180,7 +180,7 @@ def get_gpu_memory_usage():
     return memory_free, memory_used
 
 
-def get_model_memory_usage(model, batch_size=1):
+def get_model_memory_usage(model, unit, batch_size=1):
     shapes_mem_count = 0
     internal_model_mem_count = 0
     for layer in model.layers:
@@ -208,14 +208,27 @@ def get_model_memory_usage(model, batch_size=1):
 
     total_memory = number_size * (batch_size * shapes_mem_count + trainable_count + non_trainable_count)
 
-    model_bytes = int(total_memory + internal_model_mem_count)
+    model_bytes = int(total_memory + internal_model_mem_count) #bytes
 
-    return model_bytes
+    unit_dict = {
+        "kibi": 1,
+        "mebi": 2,
+        "gibi": 3
+    }
+
+    if unit in unit_dict:
+        model_size = int(model_bytes/(1024 ** unit_dict[unit]))
+    else:
+        unit = "byte"
+        model_size = model_bytes
+
+    return model_size
 
 
-def get_max_batch_size(model):
+def get_max_batch_size(model, unit="byte"):
+    #Unit: "byte", "kibi", "mebi", "gibi"
     _, gpu_used = get_gpu_memory_usage()
-    model_size = get_model_memory_usage(model)
+    model_size = get_model_memory_usage(model, unit=unit)
     print(f"GPU memory allocated: {gpu_used}")
     print(f"Model size: {model_size}")
 
