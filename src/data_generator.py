632d642db0c3fefe28accb30462c15d2e142a00d
satusky <50336340+satusky@users.noreply.github.com>
2022-10-29 15:08:17 -0400

    Add files via upload

--- src/data_generator.py
+++ src/data_generator.py
@@ -189,11 +189,6 @@ class ImageSet:
         elif self.mode == "segment":
             label_masks = sitk.ReadImage(self.labels[file_index])
             img_label = sitk.GetArrayFromImage(label_masks)
-            
-            if len(img_label.shape) == 4:
-                label_masks = self._rgb_to_gray(label_masks)
-                img_label = sitk.GetArrayFromImage(label_masks)
-                
             img_label = np.expand_dims(img_label, 3)
             img_label = tf.image.resize(img_label, [height,width]).numpy()
 
@@ -218,3 +213,176 @@ class ImageSet:
         I = 1/255.0*(0.2989*channels[0] + 0.5870*channels[1] + 0.1140*channels[2])
 
         return sitk.Cast(sitk.RescaleIntensity(I), sitk.sitkUInt8)
+
+
+class VoxelSet:
+    """A 3D image stack dataset for batch generation in TensorFlow"""
+    def __init__(self, images, labels, mode, input_shape=None, index_first=False):
+        """
+        Constructs an ImageSet from a list of stack files.
+
+        Attributes:
+            images : list
+                List of image file paths
+            labels : list
+                List of labels
+            index_first : bool
+                Defaults to False. Set True if the depth index
+                is first in the image size (not common)
+            count : int
+                Number of images in all stacks
+            min_depth : int
+                Minimum image depth in set
+            min_height : int
+                Minimum image height in set
+            min_width : int
+                Minimum image width in set
+            mode : str
+                Model mode (classify, segment)
+            input_shape : tuple
+                (depth, height, width, channel) of 3D image
+        """
+        self.images = images
+        self.labels = labels
+        self.input_shape = input_shape
+        self.index_first = index_first
+        self.count = None
+        self.min_depth = None
+        self.min_height = None
+        self.min_width = None
+        self.mode = mode
+        self.get_sizes()
+
+
+    def get_sizes(self):
+        file_size_list = 0
+        reader = sitk.ImageFileReader()
+
+        width_index = 1 if self.index_first else 0
+        height_index = 2 if self.index_first else 1
+        depth_index = 0 if self.index_first else -1
+        min_height = None
+        min_width = None
+        min_depth = None
+
+        for image in self.images:
+            reader.SetFileName(image)
+            reader.LoadPrivateTagsOn()
+            reader.ReadImageInformation()
+            img_shape = reader.GetSize()
+
+            # Get min height/width/depth
+            if min_height is None:
+                min_height = img_shape[height_index]
+            elif img_shape[height_index] < min_height:
+                min_height = img_shape[height_index]
+
+            if min_width is None:
+                min_width = img_shape[width_index]
+            elif img_shape[width_index] < min_width:
+                min_width = img_shape[width_index]
+
+            if min_depth is None:
+                min_depth = img_shape[depth_index]
+            elif img_shape[depth_index] < min_depth:
+                min_depth = img_shape[depth_index]
+
+            #file_size_list.append(img_shape[depth_index]) # (554, 512, 512)
+            file_size_list += 1
+
+        #self.count = sum(file_size_list)
+        self.count = file_size_list
+        self.min_height = min_height
+        self.min_width = min_width
+        self.min_depth = min_depth
+
+
+    def generate_dataset(self):
+        """
+        This is a custom data generator for SimpleITK image stacks.
+        'Index first' is relative to the SimpleITK image; if it is False, the first
+        position will be the depth index of the NumPy array because of
+        the shape convention difference.
+        """
+        depth = self.input_shape[0]
+        height = self.input_shape[1]
+        width = self.input_shape[2]
+        depth_index = -1 if self.index_first else 0
+
+        # Load first stack
+        file_index = 0
+
+        # Loop indefinitely
+        while True:
+            img_array, img_label, file_index = self.process_next_stack(file_index, height, width, depth)
+
+            # Extract slice and label
+            if self.index_first:
+                batch_array = img_array[..., slice_num]
+            else:
+                batch_array = img_array[slice_num, ...]
+
+            if self.mode == "classify":
+                batch_label = img_label
+            elif self.mode == "segment":
+                if self.index_first:
+                    batch_label = img_label[..., slice_num]
+                else:
+                    batch_label = img_label[slice_num, ...]
+
+            # Convert to tensor
+            batch_array = np.array(batch_array)
+            batch_tensor = tf.convert_to_tensor(batch_array)
+            batch_label = np.array(batch_label)
+            batch_label = tf.convert_to_tensor(batch_label)
+
+            # Yield to the calling function
+            yield (batch_tensor, batch_label)
+
+
+    def process_next_stack(self, file_index, height, width, depth):
+        # Load stack file
+        img = sitk.ReadImage(self.images[file_index])
+        img_array = sitk.GetArrayFromImage(img)
+        img_array = tf.image.resize(img_array, [height,width]).numpy()
+        #temp_array = sitk.GetArrayFromImage(img)
+        #img_array = np.ndarray((depth, height, width))
+        #for i in range(temp_array.shape[0]):
+        #    img_array[i] = tf.image.resize(temp_array[i], [height,width]).numpy()
+
+        # Normalize 0-1 and get label
+        img_array = (img_array - np.min(img_array))/(np.max(img_array) - np.min(img_array))
+        # Pull label
+
+        if self.mode == "classify":
+            img_label = self.labels[file_index]
+        elif self.mode == "segment":
+            label_masks = sitk.ReadImage(self.labels[file_index])
+            img_label = sitk.GetArrayFromImage(label_masks)
+            img_label = tf.image.resize(img_label, [height,width]).numpy()
+            #temp_label = sitk.GetArrayFromImage(label_masks)
+            #img_label = np.ndarray((depth, height, width))
+            #for i in range(temp_array.shape[0]):
+            #    img_label[i] = tf.image.resize(temp_label[i], [height,width]).numpy()
+
+        while img_array.shape[0] < depth:
+            np.append(img_array, np.zeros((1, img_array.shape[1], img_array.shape[2])), axis=0)
+            np.append(img_label, np.zeros((1, img_label.shape[1], img_label.shape[2])), axis=0)
+
+        if img_array.shape[0] > depth:
+            depth_diff = img_array.shape[0] - depth
+            top_crop = depth_diff // 2
+            bottom_crop = depth_diff - top_crop
+            img_array = img_array[top_crop + 1:bottom_crop]
+            img_label = img_label[top_crop + 1:bottom_crop]
+
+        img_array = np.expand_dims(img_array, 3)
+        img_label = np.expand_dims(img_label, 3)
+
+        # If not at the end of the file list, queue the next stack; otherwise loop to the beginning
+        if file_index < len(self.images)-1:
+            file_index += 1
+        else:
+            file_index = 0
+
+        return img_array, img_label, file_index
